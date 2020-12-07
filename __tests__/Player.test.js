2e55b4aaed55250688644da97e2fab2d3f8216f9
nathan-potts41 <pottsandpans41@hotmail.com>
2020-12-07 12:19:50 -0600

    adding properties to player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -10,7 +10,7 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
-    
+
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
@@ -35,3 +35,31 @@ test('gets inventory from player or returns false', () => {
     expect(player.getInventory()).toEqual(false);
 });
 
+test("gets player's health value", () => {
+    const player = new Player("Dave");
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+})
+
+test('subtracts from players health', () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
